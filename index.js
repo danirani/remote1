b78b1ff0e40a34d70b93d18ca81d8c7f1e819b37
Dan Irani <dan.irani@fastdelta.com>
2020-12-04 06:19:22 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const layout = "abcde123fghij456klmno789pqrst.@0uvwxyz_/";
+const coordinate = {row:1,col:1};
+
+const tvRemote = word => {
+    console.log(`Received '${word}'`);
+
+    [...word].reduce( (total,value) => {
+
+
+    },0)
+    ;
+
+    return 36;
+}
+
+tvRemote("codewars");
+
+module.exports = {tvRemote};
+
+
---
+++ index.test.js
@@ -0,0 +1,17 @@
+const {tvRemote} = require("./index.js");
+
+describe("Remote 1 Test", () => {
+    test('tvRemote Test', () => {
+        expect(tvRemote("codewars")).toBe(36);
+    });
+});
+
+test('misc', () => {
+  expect(tvRemote("does")).toBe(16);
+  expect(tvRemote("your")).toBe(23);
+  expect(tvRemote("solution")).toBe(33);
+  expect(tvRemote("work")).toBe(20);
+  expect(tvRemote("for")).toBe(12);
+  expect(tvRemote("these")).toBe(27);
+  expect(tvRemote("words")).toBe(25);
+});
